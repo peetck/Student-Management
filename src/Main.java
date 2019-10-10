4b80ed2db2f6555c995c12826cadca3ab555908d
peetck <peekung602@gmail.com>
2019-10-10 22:10:15 +0700

    :+1:

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
-import com.mongodb.*;
-
 public class Main{
     public static void main(String[] args) {
-        /* try{
-            DBCollection coll = null;
-            Mongo s = new Mongo("localhost", 27017);
-            DB b = s.getDB("test");
-            coll = b.getCollection("name");
-            DBCursor curs = coll.find();
-            while (curs.hasNext()){
-                DBObject n = curs.next();
-                System.out.println(n.get("name"));
-                System.out.println(n.get("age"));
-                System.out.println(n.get("test"));
-            }
-        }
-        catch (Exception ex){
 
-        } */
-        System.out.println("Hello world!");
     }
 }
