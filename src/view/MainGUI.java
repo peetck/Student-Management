62fb44abaf6cd92bbb66f5908919b9ff79549ba0
peetck <peekung602@gmail.com>
2019-10-12 01:33:12 +0700

    :up:

--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -7,6 +7,7 @@ public class MainGUI{
     private JPanel p1, p2, p3, p4;
     private JLabel l1, l2;
     private JTextField username, password;
+    private GridBagConstraints gbc;
     public MainGUI(){
         fr = new JFrame("Student Management");
         loginButton = new JButton("Login");
@@ -16,18 +17,32 @@ public class MainGUI{
         username = new JTextField(20);
         password = new JTextField(20);
         p1 = new JPanel();
-        p1.setLayout(new GridBagLayout());
+        gbc = new GridBagConstraints();
 
+        p1.setLayout(new GridBagLayout());
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         p1.add(l1);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
         p1.add(username);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
         p1.add(l2);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
         p1.add(password);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
         p1.add(loginButton);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
         p1.add(registerButton);
 
         fr.add(p1);
         fr.setSize(800, 500);
         fr.setVisible(true);
+        fr.setResizable(false);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public JButton getLoginButton(){
