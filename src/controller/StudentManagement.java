d056e9d87db5636896e92d6038b662060449c86b
peetck <peekung602@gmail.com>
2019-10-11 18:20:09 +0700

    :up:

--- src/controller/StudentManagement.java
+++ src/controller/StudentManagement.java
@@ -1,13 +1,44 @@
 package controller;
 import view.MainGUI;
 import java.awt.event.*;
+import com.mongodb.*;
 public class StudentManagement{
     private MainGUI gui;
+    private Mongo connect;
+    private DB db;
+    private DBCollection user;
     public StudentManagement(){
+        try{
+            System.out.println("Connecting to mongoDB...");
+            connect = new Mongo("localhost", 27017);
+            db = connect.getDB("StudentManagement");
+
+            /* if (user.findOne() == null){
+
+            } */
+            /* BasicDBObject newProduct = new BasicDBObject();
+            newProduct.put("productID", "productID");
+            newProduct.put("productName", "productName");
+            test.insert(newProduct); */
+            System.out.println("Connected!!");
+        }
+        catch (Exception e){
+            System.out.println("Can't Connect to Database!");
+            System.exit(0);
+        }
         gui = new MainGUI();
-        gui.getBtn1().addActionListener(new ActionListener(){
+        gui.getLoginButton().addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                System.out.println("click");
+                System.out.println("Login clicked!");
+                user = db.getCollection("user");
+                if (user.findOne() == null){
+                    System.out.println("Wrong username or password");
+                }
+            }
+        });
+        gui.getRegisterButton().addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
             }
         });
     }
--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -3,19 +3,51 @@ import java.awt.*;
 import javax.swing.*;
 public class MainGUI{
     private JFrame fr;
-    private JPanel p1;
-    private JButton btn1;
+    private JButton loginButton, registerButton;
+    private JPanel p1, p2, p3, p4;
+    private JLabel l1, l2;
+    private JTextField username, password;
     public MainGUI(){
         fr = new JFrame("Student Management");
-        btn1 = new JButton("Welcom to Student ManageMent System");
+        loginButton = new JButton("Login");
+        registerButton = new JButton("Register");
+        l1 = new JLabel("User name : ");
+        l2 = new JLabel("Password : ");
+        username = new JTextField("");
+        password = new JTextField("");
         p1 = new JPanel();
-        fr.add(btn1);
-        fr.setSize(1200, 800);
+        p2 = new JPanel();
+        p3 = new JPanel();
+        p4 = new JPanel();
+        p1.setLayout(new GridLayout(3, 1));
+        p2.setLayout(new GridLayout(1, 2));
+        p3.setLayout(new GridLayout(1, 2));
+
+        p2.add(l1);
+        p2.add(username);
+        p3.add(l2);
+        p3.add(password);
+
+        p1.add(p2);
+        p1.add(p3);
+
+        p4.add(loginButton);
+        p4.add(registerButton);
+        p1.add(p4);
+
+        fr.add(p1, BorderLayout.CENTER);
+        fr.add(new JPanel(), BorderLayout.EAST);
+        fr.add(new JPanel(), BorderLayout.SOUTH);
+        fr.add(new JPanel(), BorderLayout.WEST);
+        fr.add(new JPanel(), BorderLayout.NORTH);
+        fr.setSize(800, 500);
         fr.setVisible(true);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
-    public JButton getBtn1(){
-        return this.btn1;
+    public JButton getLoginButton(){
+        return this.loginButton;
+    }
+    public JButton getRegisterButton(){
+        return this.registerButton;
     }
 }
