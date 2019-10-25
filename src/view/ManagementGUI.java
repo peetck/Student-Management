21528a6ae0dbf074f0d5d1a855101fc7b80d204c
peetck <peekung602@gmail.com>
2019-10-25 23:33:02 +0700

    ++

--- src/view/ManagementGUI.java
+++ src/view/ManagementGUI.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import javax.swing.*;
 public class ManagementGUI{
     private JDesktopPane desktop;
-    private JPanel menu, program;
+    private JPanel menu, program, topmenu, bottommenu;
     private MyPanel menu1, menu2, menu3, menu4;
     private MyStudentGUI mystudent;
     private AddDeleteStudentGUI add_delete;
@@ -15,21 +15,29 @@ public class ManagementGUI{
         program = new JPanel();
         program.setLayout(new GridLayout(1, 1));
         mystudent = new MyStudentGUI();
-
         score = new ScoreGUI();
         add_delete = new AddDeleteStudentGUI();
         setting = new SettingGUI();
+        topmenu = Helper.createPanel("");
+        bottommenu = Helper.createPanel("");
+        
         menu1 = Helper.createPanel("images/menu/List.jpg", "menu");
         menu2 = Helper.createPanel("images/menu/Score.jpg", "menu");
         menu3 = Helper.createPanel("images/menu/AddDelete.jpg", "menu");
         menu4 = Helper.createPanel("images/menu/Setting.jpg", "menu");
         
         
-
+        topmenu.setLocation(0, 0);
+        topmenu.setSize(300, 130);
+        topmenu.setBackground(Color.lightGray);
+        
+        bottommenu.setLocation(0, 590);
+        bottommenu.setSize(300, 110);
+        bottommenu.setBackground(Color.PINK);
         
         menu.setLayout(new GridLayout(4, 1));
-        menu.setLocation(0, 0);
-        menu.setSize(300, 700);
+        menu.setLocation(0, 130);
+        menu.setSize(300, 460);
         menu.setVisible(true);
         
         menu.add(menu1);
@@ -43,7 +51,9 @@ public class ManagementGUI{
         program.add(mystudent.getPanel());
         
         desktop.setVisible(true);
+        desktop.add(topmenu);
         desktop.add(menu);
+        desktop.add(bottommenu);
         desktop.add(program);
         
     }
